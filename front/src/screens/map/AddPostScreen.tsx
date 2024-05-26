ea9586b3d810759d395b37876c107536d28fdde2
jaehyeon <122436486+dubu777@users.noreply.github.com>
2024-05-26 16:16:40 +0900

    [FE] feat: 구글 맵 역 지오코딩

--- front/src/screens/map/AddPostScreen.tsx
+++ front/src/screens/map/AddPostScreen.tsx
@@ -2,8 +2,9 @@ import AddPostHeaderRight from '@/components/AddPostHeaderRight';
 import CustomButton from '@/components/CustomButton';
 import InputField from '@/components/InputField';
 import {colors, mapNavigations} from '@/constants';
-import { useMutateCreatePost } from '@/hooks/queries/useMutateCreatePost';
+import useMutateCreatePost from '@/hooks/queries/useMutateCreatePost';
 import useForm from '@/hooks/useForm';
+import useGetAddress from '@/hooks/useGetAddress';
 import {MapStackParamList} from '@/navigations/stack/MapStackNavigator';
 import { MarkerColor } from '@/types/domain';
 import { validateAddPost } from '@/utils';
@@ -21,6 +22,7 @@ function AddPostScreen({route, navigation}: AddPostScreenProps) {
   const {location} = route.params;
   const descriptionRef = useRef<TextInput | null>(null);
   const createPost = useMutateCreatePost();
+  
   const addPost = useForm({
     initialValue: {
       title: '',
@@ -30,7 +32,7 @@ function AddPostScreen({route, navigation}: AddPostScreenProps) {
   });
   const [markerColor, setMarkerColor] = useState<MarkerColor>('RED')
   const [score, setScore] = useState(5)
-  const [address, setAdress] = useState('')
+  const address = useGetAddress(location)
 
   const handleSubmit = () => {
     const body = {
@@ -58,10 +60,14 @@ function AddPostScreen({route, navigation}: AddPostScreenProps) {
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.contentContainer}>
         <View style={styles.inputContainer}>
-          <InputField value='' disabled icon={
-            <Octicons name='location' size={16} color={colors.GRAY_500}/>
-          }/>
-          <CustomButton variant='outlined' size='large' label='날짜 선택'/>
+          <InputField
+            value={address}
+            disabled
+            icon={
+              <Octicons name="location" size={16} color={colors.GRAY_500} />
+            }
+          />
+          <CustomButton variant="outlined" size="large" label="날짜 선택" />
           <InputField
             {...addPost.getTextInputProps('title')}
             error={addPost.errors.title}
