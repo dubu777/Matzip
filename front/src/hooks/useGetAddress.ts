fed4c8b09671900f5c58022767283e820e26bb1f
jaehyeon <122436486+dubu777@users.noreply.github.com>
2024-06-04 23:05:44 +0900

    [FE] feat: 피드 포스트 api 개발

--- front/src/hooks/useGetAddress.ts
+++ front/src/hooks/useGetAddress.ts
@@ -3,6 +3,7 @@ import {useEffect, useState} from 'react';
 import type {LatLng} from 'react-native-maps';
 
 import {errorMessages} from '@/constants';
+import Config from 'react-native-config';
 
 function useGetAddress(location: LatLng) {
   const {latitude, longitude} = location;
@@ -12,7 +13,7 @@ function useGetAddress(location: LatLng) {
     (async () => {
       try {
         const {data} = await axios.get(
-          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&result_type=street_address|route|political&key=AIzaSyA791TYW0_QUKA1ADaPXb6CyVZVxt4mMlQ&language=ko`,
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&result_type=street_address|route|political&key=${Config.GOOGLE_API_KEY}&language=ko`,
         );
         const address = data.results.length
           ? data.results[0].formatted_address
